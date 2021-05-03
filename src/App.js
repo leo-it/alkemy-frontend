57b6f7eb3fe1be46729fe6968228182f131ee5b6
Leonardo Sainz <leo.database92@gmail.com>
2021-05-02 19:41:58 -1200

    descargue dependencias para enrutado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-     <h2>Alkemy App()</h2>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route path="/home" component={Home}/>
+      
+
+    </Switch>
+    </BrowserRouter>
   );
 }
 
