add3ef8ede511b78615cfbf29c4acc2fe50db26d
Leonardo Sainz <leo.database92@gmail.com>
2021-05-05 00:57:32 -1200

    loader funcionando

---
+++ src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Loader.css"
+
+ const Loader = () => {
+    return(
+        <>
+        <div className="container">
+            <div className="row">
+<div className="mx-auto lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>      
+</div>
+</div>
+  </>
+    )
+} 
+export default Loader
