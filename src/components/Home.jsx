9b78fc4b1f112dbab923c71690f961e0d6c99502
Leonardo Sainz <leo.database92@gmail.com>
2021-05-04 16:09:29 -1200

    imprime solo los 10 ultimos igresos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,31 @@
-import React,{useEffect, useState} from 'react';
-import {useFetch} from "../hooks/useFetch"
-import {Navbar} from "./Navbar"
-import ItemOperation from "./ItemOperation"
-import Balance from './Balance';
+import React, { useEffect, useState } from "react";
+import { useFetch } from "../hooks/useFetch";
+import { Navbar } from "./Navbar";
+import ItemOperation from "./ItemOperation";
+import Balance from "./Balance";
 
 const Home = () => {
-        let url="http://localhost:3000/api/"
-        let {data, isPending, error} = useFetch(`${url}operations`)
-/*         console.log(`is pending: ${isPending}`);
-        console.log(error);
-        console.log(data); */
+  let url = "http://localhost:3000/api/";
+  let { data, isPending, error } = useFetch(`${url}operations`);
 
-
-        return (
+  return (
     <>
-    <Navbar className="  "/>
-    <Balance/>
-    {!data?(
+      <Navbar className="  " />
+      <Balance />
+      {!data ? (
         <h3>cargando...</h3>
-    ):
-    //para mostrar solo 10 puedo hacer un for de 10 en reverse()
+      ) : (
         <div className="text-center">
-            {data.operations.map((el)=>
-            <ItemOperation key={el._id} dataEl={el} />
-      )}</div>
-
-    }
+          {data.operations
+            .slice(Math.max(data.operations.length - 10, 1))
+            .reverse()
+            .map((el, index) => (
+              <ItemOperation key={el._id} dataEl={el} />
+            ))}
+        </div>
+      )}
     </>
-    );
-    }
+  );
+};
 
 export default Home;
